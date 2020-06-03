a2aecac248041051472e747f4493b1370cbf39bb
Alex <a8dymov@gmail.com>
2020-06-03 20:01:26 +0500

    filled About and Contact components with content and added some styling

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -5,7 +5,18 @@ import './about.styles.scss';
 const About = () => {
   return (
     <div className='about'>
-      <span>About</span>
+      <div className='about__image'>
+      </div>
+      <div className='about__description'>
+        <h2>About me</h2>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non tortor convallis, varius arcu sed, lacinia lectus. Vestibulum dignissim arcu non felis congue pulvinar. Aenean convallis faucibus tincidunt. Nulla viverra, tellus vel auctor pharetra, quam diam commodo nisl, pulvinar consectetur massa diam sit amet enim. Ut tempor, turpis ac sollicitudin consequat, enim lorem faucibus ante, dapibus cursus lorem enim pharetra enim. Vestibulum eget metus libero. Pellentesque vel rhoncus dui. Sed et mollis tortor. Etiam molestie sem sed sem scelerisque, eu pellentesque risus mattis.
+
+      Ut non nisl auctor, scelerisque risus ut, blandit velit. Curabitur ac scelerisque leo. Pellentesque neque ligula, tincidunt congue faucibus sit amet, fermentum id mauris. Morbi quis sagittis tellus. Aenean finibus tortor vitae bibendum mollis. Donec mauris nulla, efficitur non malesuada ut, finibus ut nulla. Sed ligula nulla, vulputate vel blandit eget, congue a dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec ac risus at eros aliquet ullamcorper. Nunc tempus augue quis hendrerit laoreet. Interdum et malesuada fames ac ante ipsum primis in faucibus. In tempor tempor tellus, vel hendrerit mauris.
+
+      Nulla fermentum, ex sit amet elementum venenatis, est ante bibendum neque, non consequat libero lorem quis diam. Etiam at tellus ante. Sed varius turpis magna, quis efficitur est gravida sit amet. Praesent consectetur risus vitae ipsum rutrum, sit amet rhoncus elit tempor. Ut et dui ut urna lacinia dignissim a et neque. Pellentesque sit amet justo at velit rutrum pretium. Aliquam ac imperdiet lacus. Pellentesque in dolor dapibus, tincidunt felis at, congue lacus.
+
+      Maecenas nec diam a nunc sollicitudin euismod eu nec arcu. Integer ultricies augue orci, in convallis lacus aliquet sed. Sed eu molestie elit, ac iaculis nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec posuere egestas magna eget hendrerit. Etiam eget tincidunt lacus. Fusce vel metus magna. Curabitur euismod eros non erat eleifend, eget rhoncus odio iaculis. Ut pretium consequat nibh at lobortis. Nullam varius eu felis sed aliquam. Ut varius, tortor nec consectetur posuere, ligula tellus ullamcorper nulla, eu cursus velit lorem et ante. Sed pretium ac augue ut ullamcorper. Maecenas tellus felis, sodales vel luctus dictum, pharetra id arcu. Ut blandit neque at aliquam dignissim. In arcu dui, vestibulum a venenatis eu, vestibulum sed est. Mauris in imperdiet sapien.
+      </div>
     </div>
   );
 };
--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -5,7 +5,12 @@ import './contact.styles.scss';
 const Contact = () => {
   return (
     <div className='contact'>
-      <span>Contact</span>
+      <h2>Feel free to contact me with followings</h2>
+      <div className='contact__links'>
+        <span><i className="fab fa-vk"></i></span>
+        <span><i className="fab fa-twitter"></i></span>
+        <span><i className="fas fa-envelope"></i></span>
+      </div>
     </div>
   );
 };
