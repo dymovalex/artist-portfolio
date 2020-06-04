ad779f7e20f2200e3d358e2a9a84cc50fba51d0e
Alex <a8dymov@gmail.com>
2020-06-04 20:43:04 +0500

    making design responsive for mobile devices

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import './header.styles.scss';
 const Header = () => {
   return (
     <div className='header'>
-      <div></div>
+      <div className='header__blank-div'></div>
       <div className='header__logo'>
         <Link to='/'>
           <span>Christina Dymova</span>
