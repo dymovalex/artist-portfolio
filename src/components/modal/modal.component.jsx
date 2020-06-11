c787be1c1b50e01766efa6370dd0e38fb3e4ba3f
unknown <a8dymov@gmail.com>
2020-06-11 17:07:56 +0500

    new branch: modal-improvement. Added the ability to open images to the width and height of the original

--- src/components/modal/modal.component.jsx
+++ src/components/modal/modal.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import { ModalContext } from '../../providers/modal.provider';
 import { AppContext } from '../../providers/app.provider';
@@ -6,11 +6,44 @@ import { AppContext } from '../../providers/app.provider';
 import './modal.styles.scss';
 
 const Modal = () => {
-  const { modalVisibility, setModalVisibility, imageUrl } = useContext(ModalContext);
+  const { modalVisibility, setModalVisibility, imageUrl, originSizeOfAnImage, setOriginSizeOfAnImage } = useContext(ModalContext);
   const { setOverflowHidden } = useContext(AppContext);
 
+  const [imageX, setImageX] = useState(0);
+  const [imageY, setImageY] = useState(0);
+  
+  const [topIndent, setTopIndent] = useState(0);
+  const [leftIndent, setLeftIndent] = useState(0);
+
+  useEffect(() => {
+    const image = document.querySelector('.inner__image');
+    //console.log(image.offsetWidth, image.offsetHeight);
+    setImageX(image.offsetWidth);
+    setImageY(image.offsetHeight);
+  }, [originSizeOfAnImage])
+
+  const handleMouseMove = (e) => {
+    if(!originSizeOfAnImage) return;
+    //setLeftIndent()
+    console.log(window.innerWidth);
+    console.log(e.clientX);
+    setTopIndent(- imageY / window.innerHeight * e.clientY);
+    setLeftIndent(- imageX / window.innerWidth * e.clientX);
+  };
+
+  const handleImgClick = (e) => {
+    if(!originSizeOfAnImage) {
+      setOriginSizeOfAnImage(true);
+    } else {
+      setOriginSizeOfAnImage(false);
+    }
+    console.log(e.currentTarget);
+    console.log(e.currentTarget.offsetWidth, e.currentTarget.offsetHeight);
+  };
+
   return (
-    <div className={`modal ${modalVisibility ? 'visible' : ''}`}>
+    <div className={`modal ${modalVisibility ? 'visible' : ''}`}           onClick={handleImgClick}
+    onMouseMove={handleMouseMove}>
       <div className='inner'>
         <div
           className='inner__close-button'
@@ -21,7 +54,15 @@ const Modal = () => {
         >
           <i className="fas fa-times" ></i>
         </div>
-        <img src={imageUrl} alt='full screen' />
+        <img
+          src={imageUrl}
+          alt='full screen'
+          className={`${originSizeOfAnImage ? 'origin-size' : ''} inner__image`}
+          style={{
+            top: `${topIndent}px`,
+            left: `${leftIndent}px`
+          }}
+        />
       </div>
     </div>
   );
--- src/providers/modal.provider.jsx
+++ src/providers/modal.provider.jsx
@@ -5,14 +5,17 @@ export const ModalContext = createContext();
 const ModalProvider = ({ children }) => {
   const [modalVisibility, setModalVisibility] = useState(false);
   const [imageUrl, setImageUrl] = useState('');
+  const [originSizeOfAnImage, setOriginSizeOfAnImage] = useState(false);
 
   return (
     <ModalContext.Provider
       value={{
         modalVisibility,
         imageUrl,
+        originSizeOfAnImage,
         setModalVisibility,
         setImageUrl,
+        setOriginSizeOfAnImage,
       }}
     >
       {children}
